a5d4a5a841f1c6f79a0fcfa2c8bee4c5262c71af
Arjan Zuidema <arjan@myalbum.com>
2019-08-21 11:50:50 +0200

    Ignore debug android manifest

--- local-cli/core/config/android/findManifest.js
+++ local-cli/core/config/android/findManifest.js
@@ -10,7 +10,7 @@ const path = require('path');
 module.exports = function findManifest(folder) {
   const manifestPath = glob.sync(path.join('**', 'AndroidManifest.xml'), {
     cwd: folder,
-    ignore: ['node_modules/**', '**/build/**', 'Examples/**', 'examples/**'],
+    ignore: ['node_modules/**', '**/build/**', 'Examples/**', 'examples/**', '**/debug/**'],
   })[0];
 
   return manifestPath ? path.join(folder, manifestPath) : null;
